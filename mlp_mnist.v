84cb2b65de2515d818fb6145f44538f9f4500c2c
srwasabi <henriquelima0207@gmail.com>
2026-03-05 14:11:19 -0300

    fixing PE matrix and testing it

--- mlp_mnist.v
+++ mlp_mnist.v
@@ -9,6 +9,7 @@ module MAC # (parameter tam = 16)
         input [tam-1:0] w_in,
         input clk,
         input rst,
+        input en,
         output reg [tam-1:0] mac_out
     );
 
@@ -20,8 +21,8 @@ module MAC # (parameter tam = 16)
         else mac_out <= wire_sum;
     end
     
-    multi16 utt0 ( .a(data_in), .b(w_in), .en(1'b1), .result(wire_mult));
-    sum16 sum0 (.a(mac_out), .b(wire_mult), .result(wire_sum), .en(1'b1));
+    multi16 utt0 ( .a(data_in), .b(w_in), .en(en), .result(wire_mult));
+    sum16 sum0 (.a(mac_out), .b(wire_mult), .result(wire_sum), .en(en));
 
 
 
@@ -31,10 +32,11 @@ endmodule
 
 module PE # (parameter tam = 16)
     (
-        input [tam-1:0] bus_in,
+        input [tam-1:0] data_in,
         input [tam-1:0] bus_w,
         input clk,
         input rst,
+        input en,
         // PIPELINE
         //output reg [tam-1:0] bus_out,
         //output reg [tam-1:0] w_out,
@@ -42,7 +44,7 @@ module PE # (parameter tam = 16)
     );
 
 
-    MAC mac_unit (.data_in(bus_in), .w_in(bus_w), .clk(clk), .rst(rst), .mac_out(acc_out));
+    MAC mac_unit (.data_in(data_in), .w_in(bus_w), .clk(clk), .rst(rst), .mac_out(acc_out), .en(en));
 
 endmodule
 
@@ -50,17 +52,17 @@ endmodule
 
 module matrix_PE # (parameter tam = 16, parameter layer = 100)
     (
-        input [tam-1:0] bus_input,
-        input [tam-1:0] bias,
+        input [tam-1:0] bus_input, //bias include
         input [layer-1:0][tam-1:0] bus_weights,
         input clk,
+        input [layer-1:0] PE_en,
         input rst,
         output [layer-1:0][tam-1:0] acc_out
     );
 
     genvar i;
     for (i=0; i < layer; i = i + 1) begin: PEs
-        PE # (.tam(tam)) pe_unit (.bus_in(bus_input), .bus_w(bus_weights[i]), .clk(clk), .rst(rst), .acc_out(acc_out[i]));
+        PE # (.tam(tam)) pe_unit (.data_in(bus_input), .bus_w(bus_weights[i]), .clk(clk), .rst(rst), .acc_out(acc_out[i]), .en(PE_en[i]));
     end
 
 endmodule
@@ -73,3 +75,4 @@ module moduleName #(parameters)
     );
     
 endmodule
+*/
--- mlp_mnist_tb.v
+++ mlp_mnist_tb.v
@@ -1,78 +1,71 @@
 `include "mlp_mnist.v"
+
 `timescale 1ns/100ps
 `define tam 16
-`define data_size 4
+`define layer 4
 
-/*
-module mac_tb;
+
+module matrixPE_tb;
 reg clk;
 reg rst;
-reg [(`tam-1):0] in;
-reg [(`tam-1):0] weight;
-reg [(`tam-1):0] d_out;
-wire [(`tam-1):0] out;
+reg [(`layer-1):0] [(`tam-1):0] weights_in;
+wire [(`layer-1):0] [(`tam-1):0] out;
+reg [(`tam-1):0] data;
+reg [(`layer-1):0] [(`tam-1):0] d_out;
+reg [(`layer-1):0] PE_en;
+integer i;
 
-MAC #(.tam(`tam)) mac0 (.data_in(in), .initial_weight(weight), .clk(clk), .rst(rst), .mac_out(out));
+reg last_selection; // When last selection is made, all PEs are enabled at the same time
 
-initial begin
-    //$dumpfile("mac_mlp_tb.vcd");
-    //$dumpvars(0);
-    rst = 1;
-    #1;
-    in = 16'b0100000000000000;
-    weight = 16'b0011100000000000;
-    d_out = 16'b0011110000000000;
-    #6;
-    rst = 0;
+matrix_PE # (.tam(`tam), .layer(`layer)) matrix_1 (.bus_input(data), .bus_weights(weights_in), .clk(clk), .rst(rst), .acc_out(out), .PE_en(PE_en));
 
-    $display("esperado: %b\nobtido: %b", d_out, out);
-    $finish;
+always @(posedge clk, posedge rst) begin
+    
+
+    if(rst) begin 
+        PE_en <= {`layer{1'b1}};
+        last_selection <= 0;
+    end
+    else if (last_selection)begin
+        //PE_en <= PE_en << 1; -> Pipeline enable
+        PE_en <= 0; // All PEs enabled at the same time
+    end
+    
 end
 
 initial begin
-    clk = 0;
-    forever #5 clk = ~clk; 
-end
-
-endmodule
-*/
-
-//===============================================================
-
-module PE_tb;
-reg [`tam-1:0] in;
-reg [`tam-1:0] bias;
-reg [`data_size-1:0][`tam-1:0] weights;
-reg clk;
-reg rst;
-reg [`tam-1:0] d_acc;
-wire [`tam-1:0] acc;
-
-PE # (.tam(`tam), .data_size(`data_size)) PE0 (.bus_in(in), .bias(bias), .initial_weight(weights), .clk(clk), .rst(rst), .acc_out(acc));
-
-initial begin
-    $dumpfile("pe_mlp_tb.vcd");
+    $dumpfile("matrix_pe.vcd");
     $dumpvars(0);
-    rst = 1;
-    in = 16'b0011110000000000;
-    weights[0] = 16'b0011110000000000;
-    weights[1] = 16'b0100000000000000;
-    weights[2] = 16'b0100001000000000;
-    weights[3] = 16'b0100010000000000;
-    bias = 16'b0;
-    d_acc = 16'b0100100100000000;
-    #5;
+	$display("teste");
+	rst = 1;
+    #10;
     rst = 0;
+	data = 16'b0011110000000000;
+	weights_in[0] = 16'b0100000000000000;
+	weights_in[1] = 16'b0100001000000000;
+	weights_in[2] = 16'b0100010000000000;
+	weights_in[3] = 16'b0100010100000000;
 
-    $monitor("Obtido: %b\n", acc);
-    #200;
-    $display("Esperado: %b\n", d_acc);
+	d_out[0] = 16'b0100000000000000;
+	d_out[1] = 16'b0100001000000000;
+	d_out[2] = 16'b0100010000000000;
+	d_out[3] = 16'b0100010100000000;
+    last_selection = 1;
+    #20;
+
+    
+    #30;
+	$display("2 - Obtido: %b Esperado: %b", out[0], d_out[0]);
+	$display("3 - Obtido: %b Esperado: %b", out[1], d_out[1]);
+	$display("4 - Obtido: %b Esperado: %b", out[2], d_out[2]);
+	$display("5 - Obtido: %b Esperado: %b", out[3], d_out[3]);
+
     $finish;
 end
 
 initial begin
     clk = 0;
-    forever #10 clk = ~clk;
+    forever #5 clk = ~clk;
 end
 
 endmodule
